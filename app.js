af1ad12b87f417639011e52dd541b20f542afddd
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-04-27 22:59:10 +0000

    The mesage

---
+++ app.js
@@ -0,0 +1,28 @@
+let headsortails="tails"
+ if(headsortails==="heads"){
+     console.log("heads")
+ }
+ if(headsortails==="tails"){
+     console.log("tails")
+ }
+ if(headsortails==="heads"){
+    console.log("TOSS IS HEADS")
+ }
+ else{
+    console.log("TOSS IS TAILS")
+ }
+let binary=1
+if(binary===1){
+    console.log("Binary is 1")
+}
+else{
+    console.log("binary is 0")
+}
+let color="green"
+if (color==="red") {
+    console.log("Stop at all cost or the car will get into a carcrash")
+} else if(color==="yellow"){
+    console.log("slow down kid")
+} else{
+    console.log("GO AND BE FAST")
+}
