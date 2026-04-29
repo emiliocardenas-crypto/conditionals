75a58519c13972c9d81392c4b1843f79148aa0b6
emiliocardenas-crypto <emilio.cardenas@codedbykids.org>
2026-04-29 22:17:57 +0000

    I got a 0

--- app.js
+++ app.js
@@ -26,3 +26,29 @@ if (color==="red") {
 } else{
     console.log("GO AND BE FAST")
 }
+let is_tired = false
+if (is_tired===true){
+    console.log("Get some slepp kid")
+}
+else{
+    console.log("So you are not sleppy? Don't slepp for ten years.")
+}
+let grade=0
+if (grade>=90){
+    console.log("A")
+}
+else if (grade>=80){
+    console.log("B")
+}
+else if(grade>=70){
+    console.log("C")
+}
+else if (grade>=60){
+    console.log("D")
+}
+else if (grade>0){
+    console.log("F")
+}
+else{
+    console.log("You failed son now you are going to never play with your XBOX in your life")
+}
